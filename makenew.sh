772dda21ed39508a0b69f5c8ac36201cb5d62114
Evan Sosenko <evan@pureskill.gg>
2020-12-05 23:06:38 -0800

    Add defaults to makenew.sh

--- makenew.sh
+++ makenew.sh
@@ -50,12 +50,14 @@ pureskillgg () {
   echo 'There are no defaults.'
   echo 'Example values are shown in parentheses.'
   read -p '> Package title (My Package): ' mk_title
-  read -p '> Package name (@namespace/my-package): ' mk_slug
+  read -p '> Package name (my-package): ' mk_pkg
   read -p '> Short package description (Foos and bars.): ' mk_description
   read -p '> Author name (Linus Torvalds): ' mk_author
   read -p '> Author email (linus@example.com): ' mk_email
-  read -p '> GitHub user or organization name (my-user): ' mk_user
   read -p '> GitHub repository name (my-repo): ' mk_repo
+
+  mk_user="pureskillgg"
+  mk_slug="@pureskillgg/${mk_pkg}"
 
   sed_delete README.md '9,103d'
   sed_insert README.md '9i' 'TODO'
