ea70bd59a871c258b2dddbe2d58827ba4039fa6f
Evan Sosenko <evan@pureskill.gg>
2021-07-30 15:23:39 -0700

    Run format

--- examples/get.js
+++ examples/get.js
@@ -1,17 +1,19 @@
 import { getConfig, localString } from '../index.js'
 
-export const getLocalConfig = ({ log }) => async (key) => {
-  const config = await getConfig({
-    parameters: {
-      foo: localString('FOO'),
-      bar: localString('BAR')
-    },
-    aliases: { FOO: 'alpha', BAR: 'beta' },
-    localParameters,
-    log
-  })
-  if (key) return config[key]
-  return config
-}
+export const getLocalConfig =
+  ({ log }) =>
+  async (key) => {
+    const config = await getConfig({
+      parameters: {
+        foo: localString('FOO'),
+        bar: localString('BAR')
+      },
+      aliases: { FOO: 'alpha', BAR: 'beta' },
+      localParameters,
+      log
+    })
+    if (key) return config[key]
+    return config
+  }
 
 const localParameters = { alpha: 'one', beta: 'two' }
